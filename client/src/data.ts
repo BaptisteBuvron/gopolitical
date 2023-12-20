98113326d92af608d9c411687ec24fe11feb904a
soudarsane <tsoudar21@gmail.com>
2023-12-20 23:49:17 +0100

    modification affichage semblable carte en cours

--- client/src/data.ts
+++ client/src/data.ts
@@ -15,77 +15,519 @@ export const data = {
     {"country1": "ru", "country2": "us", "kind": "enemy"}
   ],
   "territories": [
-    {
-      "x": 0,
-      "y": 0,
-      "country": "ru",
-      "variations": [
+{
+  "x": 0,
+  "y": 0,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 1,
+  "y": 0,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "food", "value": 5 }
+  ]
+},
+{
+  "x": 2,
+  "y": 0,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 0,
+  "country": "us",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 4,
+  "y": 0,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 5,
+  "y": 0,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 0,
+  "y": 1,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 1,
+  "y": 1,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "food", "value": 5 }
+  ]
+},
+{
+  "x": 2,
+  "y": 1,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 1,
+  "country": "us",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 4,
+  "y": 1,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 5,
+  "y": 1,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 0,
+  "y": 2,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 1,
+  "y": 2,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "food", "value": 5 }
+  ]
+},
+{
+  "x": 2,
+  "y": 2,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 2,
+  "country": "us",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 4,
+  "y": 2,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 5,
+  "y": 2,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 0,
+  "y": 3,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 1,
+  "y": 3,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "food", "value": 5 }
+  ]
+},
+{
+  "x": 2,
+  "y": 3,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 3,
+  "country": "us",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 4,
+  "y": 3,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 5,
+  "y": 3,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 0,
+  "y": 4,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 1,
+  "y": 4,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "food", "value": 5 }
+  ]
+},
+{
+  "x": 2,
+  "y": 4,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 4,
+  "country": "us",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 4,
+  "y": 4,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 5,
+  "y": 4,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 0,
+  "y": 5,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 1,
+  "y": 5,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "food", "value": 5 }
+  ]
+},
+{
+  "x": 2,
+  "y": 5,
+  "country": "ru",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 5,
+  "country": "us",
+  "variations": [
+      { "name": "petrol", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 4,
+  "y": 5,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 5,
+  "y": 5,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 6,
+  "y": 5,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+    "x": 0,
+    "y": 6,
+    "country": "ru",
+    "variations": [
         { "name": "petrol", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    },
-    {
-      "x": 0,
-      "y": 1,
-      "country": "ru",
-      "variations": [
+    ]
+},
+{
+    "x": 1,
+    "y": 6,
+    "country": "ru",
+    "variations": [
         { "name": "petrol", "value": 10 },
         { "name": "food", "value": 5 }
-      ]
-    },
-    {
-      "x": 1,
-      "y": 1,
-      "country": "ru",
-      "variations": [
+    ]
+},
+{
+    "x": 2,
+    "y": 6,
+    "country": "ru",
+    "variations": [
         { "name": "petrol", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    },
-    {
-      "x": 1,
-      "y": 0,
-      "country": "us",
-      "variations": [
+    ]
+},
+{
+    "x": 3,
+    "y": 6,
+    "country": "us",
+    "variations": [
         { "name": "petrol", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    },
-    {
-      "x": 2,
-      "y": 0,
-      "country": "us",
-      "variations": [
+    ]
+},
+{
+    "x": 4,
+    "y": 6,
+    "country": "us",
+    "variations": [
         { "name": "food", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    },
-    {
-      "x": 2,
-      "y": 1,
-      "country": "us",
-      "variations": [
+    ]
+},
+{
+    "x": 5,
+    "y": 6,
+    "country": "us",
+    "variations": [
         { "name": "food", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    },
-    {
-      "x": 2,
-      "y": 1,
-      "country": null,
-      "variations": [
+    ]
+},
+{
+    "x": 6,
+    "y": 6,
+    "country": "us",
+    "variations": [
         { "name": "food", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    },
-    {
-      "x": 2,
-      "y": 1,
-      "country": null,
-      "variations": [
+    ]
+},
+{
+    "x": 0,
+    "y": 7,
+    "country": "ru",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+    "x": 1,
+    "y": 7,
+    "country": "ru",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "food", "value": 5 }
+    ]
+},
+{
+    "x": 2,
+    "y": 7,
+    "country": "ru",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+    "x": 3,
+    "y": 7,
+    "country": "us",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+    "x": 4,
+    "y": 7,
+    "country": "us",
+    "variations": [
         { "name": "food", "value": 10 },
         { "name": "water", "value": 5 }
-      ]
-    }
+    ]
+},
+{
+    "x": 5,
+    "y": 7,
+    "country": "us",
+    "variations": [
+        { "name": "food", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+    "x": 1,
+    "y": 8,
+    "country": "ru",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "food", "value": 5 }
+    ]
+},
+{
+    "x": 2,
+    "y": 8,
+    "country": "ru",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+    "x": 3,
+    "y": 8,
+    "country": "us",
+    "variations": [
+        { "name": "petrol", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+    "x": 4,
+    "y": 8,
+    "country": "us",
+    "variations": [
+        { "name": "food", "value": 10 },
+        { "name": "water", "value": 5 }
+    ]
+},
+{
+  "x": 2,
+  "y": 9,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 9,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+},
+{
+  "x": 3,
+  "y": 10,
+  "country": "us",
+  "variations": [
+      { "name": "food", "value": 10 },
+      { "name": "water", "value": 5 }
+  ]
+}
+
   ]
 };
--- client/src/territories/TerritoryComponent.tsx
+++ client/src/territories/TerritoryComponent.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 import {data} from "../data";
 import {getCountryColor} from "../utilities";
-import LoggingButton from "./Details"; // Import the ButtonComponent
-
-
-
 
 function TerritoryComponent() {
-    const handleTerritoryClick = (territory: { country: any; }) => {
+    const handleTerritoryClick = (territory: { country: any;  }) => {
         console.log(`Clicked on territory: ${territory.country}`);
     };
+
     return (
-        <div className="Country-tab" style={{marginTop: "10%"}}>
+        <div className="Country-tab" style={{ marginTop: "10%" }}>
             {data["territories"].map((territory, index) => (
-                <div key={index} className="territory"
-                     style={{
-                         backgroundColor: getCountryColor(territory.country),
-                         left: `${territory.x * 20}px`,
-                         top: `${territory.y * 20}px`,
-                         width: `200px`,
-                         height: `200px`,
-                         justifyContent: "center",
-                         border: "3px solid #fff",
-                     }}
-                     onClick={() => handleTerritoryClick(territory)}
+                <div
+                    key={index}
+                    className="territory"
+                    style={{
+                        backgroundColor: getCountryColor(territory.country),
+                        left: `${territory.x * 30}px`,
+                        top: `${territory.y * 30}px`,
+                        width: `${30}px`,
+                        height: `${30}px`,
+                        position: "absolute", // Ensure absolute positioning
+                    }}
+                    onClick={() => handleTerritoryClick(territory)}
                 >
-                    <p>{territory.country}</p>
-                    <LoggingButton />
+                    1
                 </div>
             ))}
         </div>
-    )
+    );
 }
+
+
+
 
 export default TerritoryComponent;
