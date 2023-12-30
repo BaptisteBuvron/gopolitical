0b6243a1824e61625d890738f9a7db235c859460
eliot <eliotdewulf91@gmail.com>
2023-12-30 17:33:52 +0100

    ajout affichage jour de la simulation dans le header

--- client/src/components/LayoutComponent.tsx
+++ client/src/components/LayoutComponent.tsx
@@ -2,15 +2,16 @@
 import React from "react";
 import HeaderComponent from "./HeaderComponent";
 import {Outlet} from "react-router-dom";
+import {Simulation} from "../Entity";
 
-/*interface LayoutComponentProps {
+interface LayoutComponentProps {
     simulation: Simulation | undefined;
-}*/
+}
 
-function LayoutComponent() {
+function LayoutComponent({simulation}: LayoutComponentProps) {
     return (
         <div style={{minHeight: "100vh"}} className="text-bg-dark">
-            <HeaderComponent />
+            <HeaderComponent simulation={simulation}/>
             <div>
                 <Outlet />
             </div>
