1e45ef3e9359cf51edd76096ca975a5c13cfa494
soudarsane <tsoudar21@gmail.com>
2023-12-30 13:10:18 +0100

    Affichage du marché en cours, reste l'esthétique à rajouter

--- client/src/components/MarketComponent.tsx
+++ client/src/components/MarketComponent.tsx
@@ -1,46 +1,70 @@
-import React from 'react';
-import { Simulation, Market } from '../Entity';
+import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
+import { Simulation } from "../Entity";
 
 interface MarketComponentProps {
     simulation: Simulation | undefined;
 }
 
 const MarketComponent: React.FC<MarketComponentProps> = ({ simulation }) => {
-    if (!simulation) {
-        return <div>Loading...</div>;
+    if (!simulation || !simulation.environment || !simulation.environment.market) {
+        return <div>Loading...</div>; // Placeholder for loading state
     }
 
-    const marketData: Market = simulation.environment?.market;
+    const marketData = simulation.environment.market;
+    const marketPrices = marketData.prices;
+    const marketHistory = marketData.history;
+
+    // Mise des transactions dans l'ordre décroissant de la date
+    const sortedMarketHistory = marketHistory.slice().sort((a, b) => {
+        const dateA = new Date(a.dateTransaction).getTime();
+        const dateB = new Date(b.dateTransaction).getTime();
+        return dateB - dateA;
+    });
+
+    // Liste des prix du marché des différentes ressources
+    const marketPricesElements = Array.from(marketPrices.entries()).map(([resource, price], index) => (
+        <Card key={`price-${index}`} className="mb-3">
+            <Card.Body>
+                <Card.Title>Current Market Price</Card.Title>
+                <Card.Text>
+                    <strong>Resource:</strong> {resource}
+                    <br />
+                    <strong>Price:</strong> {price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    ));
+
+    // Liste des transactions
+    const sortedMarketHistoryElements = sortedMarketHistory.map((interaction, index) => (
+        <Card key={index} className="mb-3">
+            <Card.Body>
+                <Card.Title>Day {interaction.dateTransaction}</Card.Title>
+                <Card.Text>
+                    <strong>Resource:</strong> {interaction.resourceType}
+                    <br />
+                    <strong>Amount:</strong> {interaction.amount}
+                    <br />
+                    <strong>Price:</strong> {interaction.price}
+                    <br />
+                    <strong>Buyer:</strong> {interaction.buyer.agent.name}
+                    <br />
+                    <strong>Seller:</strong> {interaction.seller.agent.name}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    ));
 
     return (
-        <div>
-            <h1>Market Component</h1>
-            {marketData && (
-                <div>
-                    <h2>Market Prices</h2>
-                    <ul>
-                        {Object.entries(marketData.prices).map(([resource, price]) => (
-                            <li key={resource}>
-                                {resource}: {price}
-                            </li>
-                        ))}
-                    </ul>
-                    <h2>Market History</h2>
-                    <ul>
-                        {marketData.history.map((interaction, index) => (
-                            <li key={index}>
-                                <p>Date: {interaction.dateTransaction}</p>
-                                <p>Resource: {interaction.resourceType}</p>
-                                <p>Amount: {interaction.amount}</p>
-                                <p>Price: {interaction.price}</p>
-                                <p>Buyer: {interaction.buyer.agent.name}</p>
-                                <p>Seller: {interaction.seller.agent.name}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-        </div>
+        <Row>
+            <Col>
+                <h2>Market Prices</h2>
+                {marketPricesElements}
+                <h2>Market Transactions</h2>
+                {sortedMarketHistoryElements}
+            </Col>
+        </Row>
     );
 };
 
